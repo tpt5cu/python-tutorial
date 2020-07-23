5998e86165a52889faf14133b5b0d7588d637be1
Tuomas Talvitie <ttalvitie49@gmail.com>
2020-07-22 22:20:37 -0400

    git and raedme section added and moved

---
+++ Git and README/README-git.md
@@ -0,0 +1,8 @@
+# Markdown Formatting and Tricks
+
+## Screenshots
+
+`![Alt text](/relative/path/to/img.jpg?raw=true "Optional Title")`
+
+
+
--- language/python_369/python_369/popular_modules/glob.py
+++ language/python_369/python_369/popular_modules/glob.py
@@ -8,11 +8,22 @@ for name in glob.glob('/home/geeks/Desktop/gfg/data.txt'):
     print(name) 
   
 # Using '*' pattern  
+"""
+An asterisk (*) matches zero or more characters in a segment of a name. 
+For example, dir/*.
+"""
+
 print('\nNamed with wildcard *:') 
 for name in glob.glob('/home/geeks/Desktop/gfg/*'): 
     print(name) 
+#OR
+for name in glob.glob('/home/geeks/Desktop/gfg/file_*'): 
+    print(name) 
   
 # Using '?' pattern 
+"""The other wildcard character supported is the question mark (?). 
+It matches any single character in that position in the name. For example,"""
+
 print('\nNamed with wildcard ?:') 
 for name in glob.glob('/home/geeks/Desktop/gfg/data?.txt'): 
     print(name) 
